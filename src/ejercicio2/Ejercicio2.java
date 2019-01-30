6463b1c1787deec581fe3b17c47ec16f87c25c37
Sergio Gutierrez <sergio.gutierrez@cesjuanpablosegundo.es>
2019-01-30 11:35:34 +0100

    Version inicial

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio;
+package ejercicio2;
 
 /**
  *
  * @author Usuario DAM 1
  */
-public class Ejercicio {
+public class Ejercicio2 {
 
     /**
      * @param args the command line arguments
