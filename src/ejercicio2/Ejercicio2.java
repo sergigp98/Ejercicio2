af6d7d3263d73467434fda88251f379e7ccf9f1f
Sergio Gutierrez <sergio.gutierrez@cesjuanpablosegundo.es>
2019-01-30 12:10:35 +0100

    Version inicial

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -9,13 +9,36 @@ package ejercicio2;
  *
  * @author Usuario DAM 1
  */
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
     /**
      * @param args the command line arguments
      */
+    
+   
     public static void main(String[] args) {
         // TODO code application logic here
+        String [] diassemana= {"Lunes","Martes","Miercoles","Jueves","Viernes"};
+        String hora;
+        String aux;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introduce el dia: ");
+        
+      
+        
+        aux = sc.nextLine();
+        
+        if(aux.equals(diassemana[0])){
+            hora = sc.nextLine();
+            
+            
+        }
+        
+        
+        
     }
     
 }
